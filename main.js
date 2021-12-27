31522791a64162755164d5439ccf2718537d6842
nighfy <82936194+nighfy@users.noreply.github.com>
2021-12-27 10:12:29 +0100

    Add dummyLists module and generate dummy lists

--- main.js
+++ main.js
@@ -1,1 +1,116 @@
-alert("testing");
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/dummyLists.js":
+/*!***************************!*\
+  !*** ./src/dummyLists.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"dummyLists\": () => (/* binding */ dummyLists)\n/* harmony export */ });\n/* harmony import */ var _factories__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./factories */ \"./src/factories.js\");\n/* harmony import */ var _storage__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./storage */ \"./src/storage.js\");\n\n\n\n\n\nconst dummyLists = () => {\n    let list = [];\n    for(let i = 0; i < 10; i++) {\n        \n        list.push((0,_factories__WEBPACK_IMPORTED_MODULE_0__.todosFactory)('Default list item ' + i));\n    }\n    let listObject = (0,_factories__WEBPACK_IMPORTED_MODULE_0__.listsFactory)('defaultList', list);\n    _storage__WEBPACK_IMPORTED_MODULE_1__.storage.write(listObject);\n\n    list = [];\n    for(let i = 0; i < 10; i++) {\n        list.push((0,_factories__WEBPACK_IMPORTED_MODULE_0__.todosFactory)('Other list item ' + i));\n    }\n    listObject = (0,_factories__WEBPACK_IMPORTED_MODULE_0__.listsFactory)('otherList', list);\n    _storage__WEBPACK_IMPORTED_MODULE_1__.storage.write(listObject);\n};\n\n\n\n//# sourceURL=webpack://todo-list/./src/dummyLists.js?");
+
+/***/ }),
+
+/***/ "./src/factories.js":
+/*!**************************!*\
+  !*** ./src/factories.js ***!
+  \**************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"listsFactory\": () => (/* binding */ listsFactory),\n/* harmony export */   \"todosFactory\": () => (/* binding */ todosFactory)\n/* harmony export */ });\n\n\nconst listsFactory = (name, todos) => {\n    return { name, todos }\n};\n\nconst todosFactory = (title, description, dueDate, priority, notes, checklist) => {\n    return { title, description, dueDate, priority, notes, checklist };\n};\n\n\n\n//# sourceURL=webpack://todo-list/./src/factories.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _storage__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./storage */ \"./src/storage.js\");\n/* harmony import */ var _dummyLists__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./dummyLists */ \"./src/dummyLists.js\");\n'use scrict';\n\n\n\n\nlocalStorage.clear();\n(0,_dummyLists__WEBPACK_IMPORTED_MODULE_1__.dummyLists)();\n\nconsole.log(_storage__WEBPACK_IMPORTED_MODULE_0__.storage.read());\n\n//# sourceURL=webpack://todo-list/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/storage.js":
+/*!************************!*\
+  !*** ./src/storage.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"storage\": () => (/* binding */ storage)\n/* harmony export */ });\n\n\nlet storage = {\n    \n    write: function(listObject) {\n        const strList = JSON.stringify(listObject);\n        localStorage.setItem(listObject.name, strList);\n    },\n    read: function() {\n        let arr = [];\n        for (let i = 0; i < localStorage.length; i++)   {\n            arr.push(JSON.parse(\n                localStorage.getItem(localStorage.key(i))\n            ));\n        }\n        return arr;\n    },\n};\n\n\n\n//# sourceURL=webpack://todo-list/./src/storage.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
