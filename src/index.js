7291e81b7911a1dad72d17f340a911fd4c6cf58e
nighfy <82936194+nighfy@users.noreply.github.com>
2021-12-27 11:50:01 +0100

    Add basic HTML and display default list

--- src/index.js
+++ src/index.js
@@ -6,4 +6,25 @@ import { dummyLists } from "./dummyLists";
 localStorage.clear();
 dummyLists();
 
-console.log(storage.read());
+const lists = storage.read();
+console.log(lists);
+
+let currentList = [];
+lists.forEach((element) => {
+    if(element.name == 'defaultList') {
+        currentList = element.todos;
+    };
+});
+
+console.log(currentList);
+
+const divList = document.getElementById('list');
+const genList = document.createElement('div');
+
+currentList.forEach((element) => {
+    const tempDiv = document.createElement('div');
+    tempDiv.textContent = element.title;
+    genList.appendChild(tempDiv);
+});
+
+divList.appendChild(genList);
