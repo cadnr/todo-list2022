7b73a20a828d47cbc590d2f930ed27c1be833707
nighfy <82936194+nighfy@users.noreply.github.com>
2022-01-06 14:12:07 +0100

    Remove console logs

--- src/eventListener.js
+++ src/eventListener.js
@@ -5,7 +5,6 @@ import { listsFactory, todosFactory } from "./factories";
 
 const eventListener = () => {
         window.addEventListener('click', e => {
-            console.log(e.path);
             if (e.target.classList.contains('delete-button')) {
                 const lists = storage.read();
                 const index = e.path[1].getAttribute('data-index');
@@ -18,17 +17,14 @@ const eventListener = () => {
                 dom.updateList();
             } else if(e.target.classList.contains('add-button')) {
                 const input = document.querySelector('.add-form');
-                console.log(input.value);
                 const lists = storage.read();
                 const index = e.path[1].getAttribute('data-index');
                 let list = storage.fetchList(dom.selectedList);
-                console.log(list.todos);
                 list.todos.push(todosFactory(input.value));
                 storage.write(list);
                 dom.updateList();
             } else if(e.target.classList.contains('new-list-button')) {
                 const input = document.querySelector('.new-list-input');
-                console.log(input.value);
                 storage.newList(input.value);
                 dom.updateList();            
             } else if (e.target.hasAttribute('data-list-key')) {
@@ -45,7 +41,6 @@ const eventListener = () => {
             } else if (e.target.classList.contains('edit-button')) {
                 const divs = e.path[1].querySelectorAll(`.title-edit, .desc-edit, .title-span, 
                                 .desc-span, .save-button`);
-                console.log(divs);
                 divs.forEach((e) => { e.classList.toggle('off'); });
             } else if (e.target.classList.contains('save-button')) {
                 const lists = storage.read();
@@ -55,7 +50,6 @@ const eventListener = () => {
                 const titleInput = e.path[1].querySelector('.title-input');
                 const descInput = e.path[1].querySelector('.desc-input');
 
-                console.log(list.todos[index].title);
                 list.todos[index].title = titleInput.value;
                 list.todos[index].description = descInput.value;
                 
